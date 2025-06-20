97a835438bc29243399ef3d25ef30b2f59640230
Gabriele Alleruzzo <alleruzzo.gabriele.96@gmail.com>
2025-06-20 16:52:43 +0200

    Initial commit

---
+++ src/main/java/epicode/U5W3L5test/U5W3L5testApplication.java
@@ -0,0 +1,13 @@
+package epicode.U5W3L5test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W3L5testApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5W3L5testApplication.class, args);
+	}
+
+}
---
+++ src/test/java/epicode/U5W3L5test/U5W3L5testApplicationTests.java
@@ -0,0 +1,13 @@
+package epicode.U5W3L5test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W3L5testApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
